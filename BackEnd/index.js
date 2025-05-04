72389e4158a26b8704ea68deb5ed4fb7183a3d5a
Gekyume Serna <contatogekyume@gmail.com>
2025-05-04 13:49:10 -0300

    adiciona a rota do auth

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,21 +3,21 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://192.168.0.4:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials:true
 }));
 
-// Importe as rotas DEPOIS da configuração do CORS
 const scraperRoutes = require('./routes/scraperRoutes');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
 
-// Certifique-se que os paths estão corretos
 app.use('/api/scraper', scraperRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
