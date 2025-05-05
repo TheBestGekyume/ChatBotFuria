ee1043f2f7c9a0ddac473d531d4f1fe6e0253676
Gekyume Serna <contatogekyume@gmail.com>
2025-05-04 21:45:03 -0300

    implementa serverless

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
 const serverless = require('serverless-http');
-
-dotenv.config();
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
+// CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://192.168.0.4:5173', 'https://chatbot-furioso.netlify.app'],
+  origin: ['http://localhost:5173', 'https://chatbot-furioso.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials:true
+  credentials: true
 }));
 
+// Rotas
 const scraperRoutes = require('./routes/scraperRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
-
 app.use('/api/scraper', scraperRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
-module.exports = serverless(app);
+// Exporte a função para o Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
